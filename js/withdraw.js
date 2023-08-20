1d55be8615c2d7b0f4211f92cda7729544995442
MD Rafee Rawshony Rizvee <113051488+rafee-rawshony@users.noreply.github.com>
2023-08-20 06:38:36 +0600

    Final Push

--- js/withdraw.js
+++ js/withdraw.js
@@ -4,33 +4,41 @@
 */
 
 // step 1
-document.getElementById('btn-withdraw').addEventListener('click', function(){
+document.getElementById('btn-withdraw').addEventListener('click', function () {
     // step 2
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    console.log(newWithdrawAmount);
+
+    // Step-7: Clear the deposit field
+    withdrawField.value = '';
+
+    if (isNaN(newWithdrawAmount)) {
+        alert('Ohh!! You do not input any number');
+        return;
+    }
 
     // step 3
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    // step 4
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
     // step 5 
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
-        
-    if(newWithdrawAmount > previousBalanceTotal){
+
+    if (newWithdrawAmount > previousBalanceTotal) {
         alert('Insufficient Balance');
         return;
     }
+
+    // step 4
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
     // step 6
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
-
-    // Step-7: Clear the deposit field
-    withdrawField.value = '';
 })
